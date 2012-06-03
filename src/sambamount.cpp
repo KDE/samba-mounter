4d9a96fe6df38d88ad4658e52791c20061fc88db
Alex Fiestas <afiestas@kde.org>
2012-06-03 19:59:23 +0200

    Enable remove button only if the item selected is not "new mount point"

--- src/sambamount.cpp
+++ src/sambamount.cpp
@@ -91,7 +91,7 @@ void SambaMount::currentItemChanged(QListWidgetItem* current, QListWidgetItem* p
 
     m_ui->mountInfo->setTitle(current->text());
     m_layout->setCurrentWidget(current->data(Qt::UserRole + 1).value<QWidget *>());
-//     m_ui->remoteBtn->setEnabled(current != m_newAccountItem);`
+    m_ui->remoteBtn->setEnabled(current != m_newMountItem);
 }
 
 void SambaMount::mountCreated(KConfigGroup group)
